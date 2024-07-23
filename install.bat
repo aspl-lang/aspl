1cf9610ad4f2026b4f0410e44e5f29030abfea07
Wertzui123 <46199283+Wertzui123@users.noreply.github.com>
2024-07-23 17:38:54 +0200

    installer: cleanup & fix CI/CD issues caused by rate limits in the Github API

--- install.bat
+++ install.bat
@@ -1,48 +1,56 @@
 @echo off
+setlocal enabledelayedexpansion
 
 FOR /F "tokens=*" %%g IN ('git config --get remote.origin.url') do (SET remote=%%g)
 echo %remote% | findstr /C:"github.com/aspl-lang/aspl" > nul && (
-    git "pull" "origin" "main"
+    git pull origin main
 ) || (
-    git "clone" "https://github.com/aspl-lang/aspl.git"
-    cd "aspl"
+    git clone https://github.com/aspl-lang/aspl.git
+    cd aspl
 )
 
 curl -L -o jq.exe https://github.com/stedolan/jq/releases/latest/download/jq-win64.exe
 
-(curl -s https://api.github.com/repos/aspl-lang/cd/contents/latest.txt | jq -r .content) > sha.txt
+IF DEFINED GITHUB_TOKEN (
+    SET AUTH_HEADER=Authorization: token %GITHUB_TOKEN%
+) ELSE (
+    SET AUTH_HEADER=
+)
+
+curl -s -H "%AUTH_HEADER%" https://api.github.com/repos/aspl-lang/cd/contents/latest.txt | jq -r .content > sha.txt
 certutil -decode sha.txt sha_decoded.txt
 SET /p SHA=<sha_decoded.txt
 DEL sha.txt
 DEL sha_decoded.txt
-SET CURL=curl https://api.github.com/repos/aspl-lang/cd/releases
 
-if %PROCESSOR_ARCHITECTURE% == AMD64 (
+IF "%PROCESSOR_ARCHITECTURE%"=="AMD64" (
     SET EXECUTABLE=aspl_windows_x86_64.exe
-)
-if %PROCESSOR_ARCHITECTURE% == IA64 (
+) ELSE IF "%PROCESSOR_ARCHITECTURE%"=="IA64" (
     SET EXECUTABLE=aspl_windows_x86_64.exe
-)
-if %PROCESSOR_ARCHITECTURE% == ARM64 (
+) ELSE IF "%PROCESSOR_ARCHITECTURE%"=="ARM64" (
     SET EXECUTABLE=aspl_windows_arm64.exe
-)
-if not defined EXECUTABLE (
+) ELSE (
     echo Unsupported architecture
     exit /b 1
 )
 
-(%CURL%/tags/SHA-%SHA% | jq .assets | jq ".[] | select(.name == \"%EXECUTABLE%\").id") > id.txt
+curl -H "%AUTH_HEADER%" https://api.github.com/repos/aspl-lang/cd/releases/tags/SHA-%SHA% -s | jq .assets | jq ".[] | select(.name == \"%EXECUTABLE%\").id" > id.txt
 SET /p ASSET_ID=<id.txt
-%CURL%/assets/%ASSET_ID% -LJOH "Accept: application/octet-stream"
 DEL id.txt
+echo Downloading %EXECUTABLE% (%ASSET_ID%):
+curl -H "%AUTH_HEADER%" -LJOH "Accept: application/octet-stream" https://api.github.com/repos/aspl-lang/cd/releases/assets/%ASSET_ID%
 rename %EXECUTABLE% aspl.exe
-setx PATH "%PATH%;%CD%"
 
-(%CURL%/tags/SHA-%SHA% | jq .assets | jq ".[] | select(.name == \"templates.zip\").id") > id.txt
+echo Make the `aspl` command available everywhere (by adding it to the PATH?)? (y/n): 
+set /p choice=
+if /i "%choice%"=="y" setx PATH "%PATH%;%CD%"
+
+curl -H "%AUTH_HEADER%" https://api.github.com/repos/aspl-lang/cd/releases/tags/SHA-%SHA% -s | jq .assets | jq ".[] | select(.name == \"templates.zip\").id" > id.txt
 SET /p ASSET_ID=<id.txt
 DEL id.txt
-%CURL%/assets/%ASSET_ID% -LJOH "Accept: application/octet-stream"
-tar -xf "templates.zip"
-DEL  "templates.zip"
+echo Downloading templates.zip (%ASSET_ID%):
+curl -H "%AUTH_HEADER%" -LJOH "Accept: application/octet-stream" https://api.github.com/repos/aspl-lang/cd/releases/assets/%ASSET_ID%
+tar -xf templates.zip
+DEL templates.zip
 
 DEL jq.exe
--- install.sh
+++ install.sh
@@ -6,7 +6,15 @@ else
     git clone https://github.com/aspl-lang/aspl.git
     cd aspl
 fi
-SHA=$(curl -s https://api.github.com/repos/aspl-lang/cd/contents/latest.txt -s | jq -r '.content' | base64 -d)
+
+if [ -n "$GITHUB_TOKEN" ]; then
+    AUTH_HEADER="Authorization: token $GITHUB_TOKEN"
+else
+    AUTH_HEADER=""
+fi
+
+SHA=$(curl -s -H "$AUTH_HEADER" https://api.github.com/repos/aspl-lang/cd/contents/latest.txt | jq -r '.content' | base64 -d)
+
 if [ "$(uname)" = "Darwin" ]; then
     if [ "$(uname -m)" = "x86_64" ]; then
         EXECUTABLE=aspl_macos_x86_64
@@ -34,25 +42,26 @@ else
     exit 1
 fi
 
-CURL="curl https://api.github.com/repos/aspl-lang/cd/releases"
-ASSET_ID=$(eval "$CURL/tags/SHA-$SHA -s" | jq .assets | jq '.[] | select(.name == "'$EXECUTABLE'").id')
+CURL="curl -s -H '$AUTH_HEADER' https://api.github.com/repos/aspl-lang/cd/releases"
+ASSET_ID=$(eval "$CURL/tags/SHA-$SHA" | jq .assets | jq '.[] | select(.name == "'$EXECUTABLE'").id')
 echo "Downloading $EXECUTABLE ($ASSET_ID):"
 eval "$CURL/assets/$ASSET_ID -LJOH 'Accept: application/octet-stream' --progress-bar"
 
 mv $EXECUTABLE aspl
 chmod +x aspl
 
-read -r -n 1 -p "Make aspl publicly avaiable by creating symlink in /usr/local/bin ? (Y/n): " choice
+read -r -n 1 -p "Make the \`aspl\` command available everywhere (by creating a symlink in /usr/local/bin)? (y/n): " choice
 printf "\n"
-case "$choice" in 
-  ''|y|Y ) echo "Creating link"; sudo ln -fs $PWD/aspl /usr/local/bin/aspl;;
-  * ) echo "Skiping...";;
+case "$choice" in
+'' | y | Y)
+    echo "Creating link"
+    sudo ln -fs $PWD/aspl /usr/local/bin/aspl
+    ;;
+*) echo "Skiping..." ;;
 esac
 
-CURL="curl https://api.github.com/repos/aspl-lang/cd/releases"
-ASSET_ID=$(eval "$CURL/tags/SHA-$SHA -s" | jq .assets | jq '.[] | select(.name == "templates.zip").id')
+ASSET_ID=$(eval "$CURL/tags/SHA-$SHA" | jq .assets | jq '.[] | select(.name == "templates.zip").id')
 echo "Downloading templates.zip ($ASSET_ID):"
 eval "$CURL/assets/$ASSET_ID -LJOH 'Accept: application/octet-stream' --progress-bar"
-unzip -of templates.zip
+unzip -o templates.zip
 rm templates.zip
-
